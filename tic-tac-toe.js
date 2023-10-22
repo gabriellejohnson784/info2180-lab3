94c577485353f07a9ec609a086da0f53d2394006
gabrielle johnson <gabriellejohnson784@gmail.com>
2023-10-22 15:39:52 -0500

    Added squares

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,11 +1,41 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Get all the square elements on the game board
-    const squares = document.querySelectorAll('.square');
-  
-    // Loop through each square and add the appropriate class
-    squares.forEach(square => {
-      square.classList.add("tic-tac-toe.css"); // Replace 'your-css-class-name' with the actual class you want to add
+    const Square = document.querySelectorAll('.square');
+    Square.forEach(square => {
+      square.classList.add("tic-tac-toe.css"); 
     });
   });
+
+  document.addEventListener('DOMContentLoaded', function () {
+    const squares = document.querySelectorAll('.square');
+    const state = new Array(9).fill(''); // Initialize an empty array to track the game state
+    let playerX = true; //  player X 
+    let playerY = false; //player y
+  
+    squares.forEach((square, index) => {
+      square.addEventListener('click', () => {
+        if (!square.textContent && state[index] === '') {
+          if (playerX) {
+            square.textContent = 'X'; 
+            square.classList.add('X');
+            state[index] = 'X';
+            playerX = false;
+            playerY = true;
+          } else {
+            square.textContent = 'O';
+            square.classList.add('O');
+            state[index] = 'O';
+            playerX = true;
+            playerY = false;
+          }
+  
+          // Check for win/draw
+          checkWin(state);
+        }
+      });
+    });
+  });
+  
+
+  
   
   
